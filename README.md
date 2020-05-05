11ea5af9f568ab57ad72972bb608aeeb49e398d9
halil burak cetinkaya <hburak3534@gmail.com>
2020-05-06 00:29:05 +0300

    changed connection_string.

--- README.md
+++ README.md
@@ -2,5 +2,4 @@ This is a sample REST API with Express.js and Mongoose (MongoDB Client)
 
 Do not forget to set those environment variables below before starting the server.
 
-MONGO_UNAME="Your mongodb atlas username"
-MONGO_PWD="Your mongodb atlas password"
+MONGO_CONNECTION_STRING="Your mongodb atlas cluster connection string"
--- index.js
+++ index.js
@@ -4,24 +4,10 @@ const routes = require("./routes");
 const bodyParser = require("body-parser");
 
 //removed the db url and the password from the code and converted them into env vars
-var env = process.env.NODE_ENV || "development";
-console.log(
-  "mongodb+srv://" +
-    process.env.MONGO_UNAME +
-    ":" +
-    process.env.MONGO_PWD +
-    "@cluster0-dhphg.mongodb.net/test?retryWrites=true&w=majority/acmedb"
-);
-
 mongoose
-  .connect(
-    "mongodb+srv://" +
-      process.env.MONGO_UNAME +
-      ":" +
-      process.env.MONGO_PWD +
-      "@cluster0-dhphg.mongodb.net/test?retryWrites=true&w=majority/acmedb",
-    { useNewUrlParser: true }
-  )
+  .connect(process.env.MONGO_CONNECTION_STRING + "/acmedb", {
+    useNewUrlParser: true,
+  })
   .then(() => {
     const app = express();
     app.use(bodyParser.json());
